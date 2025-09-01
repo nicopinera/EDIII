d7dbf88b8a2135ca81bf6986f3ee6855d1541551
Nicolas Piñera <nicolaspinera@gmail.com>
2025-09-01 17:08:13 -0300

    Agregado: Clases Ayarde y Gallardo

---
+++ Proyectos_MCXpresso/02_Ayarde/src/clasesPracticasAyarde.c
@@ -0,0 +1,18 @@
+/* CLASES PRACTICAS DE AYARDE
+ *
+ */
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+
+int main(void)
+{
+
+    while (1)
+    {
+    }
+    return 0;
+}
